71cbf361e24154a1ade7d6831a80cd9d5e9207d2
Brock Stone <BrockStone@fullsail.edu>
2014-09-09 23:17:09 -0400

    added new project to recent projects section

--- contact_process.php
+++ contact_process.php
@@ -1,30 +1,30 @@
-// <?php
+<?php
 
-// if(isset($_POST)) {
+if(isset($_POST)) {
 	
-// 	$to = "jocelyn@wcfdesign.com";
-// 	$subject = $_POST['subject'];
-// 	$name_field = $_POST['name'];
-// 	$email_field = $_POST['email'];
-// 	$message = $_POST['message'];
+	$to = "jocelyn.palacki@gmail.com";
+	$subject = $_POST['subject'];
+	$name_field = $_POST['name'];
+	$email_field = $_POST['email'];
+	$message = $_POST['message'];
 
-// 	$headers = 'From:'.$to."\r\n" ;
-//     $headers .='Reply-To: '. $to . "\r\n" ;
-//     $headers .='X-Mailer: PHP/' . phpversion();
-//     $headers .= "MIME-Version: 1.0\r\n";
-//     $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+	$headers = 'From: WCFdesign.com'."\r\n" ;
+    $headers .='Reply-To: '. $to . "\r\n" ;
+    $headers .='X-Mailer: PHP/' . phpversion();
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
 	 
-// 	$body = "From:".$name_field."\n E-Mail:".$email_field."\n Message:\n".$message;
+	$body = "From:".$name_field."\n E-Mail:".$email_field."\n Message:\n".$message;
  
-// 	echo "Data has been submitted to $to!";
 	
-// 	//Submit form
-// 	mail($to, $subject, $body, $headers);
+	
+	//Submit form
+	mail($to, $subject, $body, $headers);
+
+	echo "Data has been submitted to $to!";
+}else {
+ echo "There was an error!";
+}
 
 
-// }else {
-//  echo "There was an error!";
-// }
-
-
-// ?>
+?>
